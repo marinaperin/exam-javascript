480a5e274a6b5fffcc776fe8e4bc8a3fc0b102f7
Marina Perin <maryfrancisco@live.com>
2023-11-22 10:33:53 +0100

    feat | exercise 1

---
+++ exercise-1.js
@@ -0,0 +1,11 @@
+// Exercise 1
+
+const userName = prompt('Write your name');
+const userYear = Number(prompt('Write your birth year'));
+let isLeapYear;
+if (userYear%4 === 0 && userYear%100 !== 0|| userYear%400 === 0){
+    isLeapYear = 'a leap year';
+}else{
+    isLeapYear = 'not a leap year';
+}
+alert(`Hello ${userName}, your birth year is ${isLeapYear}`);
